fa0a5e7a7d36f9069a0277ca19df15fbed3e8d1a
RAHU <tiwarirahul0809@gmail.com>
2023-03-16 21:11:24 +0530

    clone compleated (data not added)

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
-import './style.css'
+import data from "./data"
+import style from "./style.css"
 
 export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+                
+            />
+        )
+    })        
+    
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
     )
 }
